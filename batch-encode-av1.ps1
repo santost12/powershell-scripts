e1c68fcb092d845b5b4816fc2c62f5dd1b4cff5f
santost12 <45154554+santost12@users.noreply.github.com>
2022-04-30 22:28:05 -0400

    Update batch-encode-av1.ps1

--- batch-encode-av1.ps1
+++ batch-encode-av1.ps1
@@ -1,5 +1,5 @@
 # Change these
-$ffmpegBinary = "C:\Users\tyler\Downloads\ffmpeg-1961492-af2b166-win64-nonfree\ffmpeg.exe"
+$ffmpegBinary = "C:\Users\tyler\bin\ffmpeg-2022-04-25-git-f2724d2b69-full_build\bin\ffmpeg.exe"
 
 $inputDir = "C:\Users\tyler\handbrake-input\"
 $outputDir = "C:\Users\tyler\handbrake-output\"
@@ -11,5 +11,5 @@ foreach ($file in Get-ChildItem -Path $inputDir)
 {
 	$finalInput = "" + $inputDir + $file + ""
 	$finalOutput = "" + $outputDir + $file + ""
-	& $ffmpegBinary -i $finalInput -c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 2 -c:a libopus -b:a 128k $finalOutput
+	& $ffmpegBinary -i $finalInput -c:v libaom-av1 -crf 30 -pix_fmt yuv420p10le -b:v 0 -cpu-used 4 -c:a libopus -b:a 128k $finalOutput
 }
